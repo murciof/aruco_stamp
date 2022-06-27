a3d63241769cd80a174e5ed8b81d65f5fb7126e1
Murcio Filho <murciofilho94@gmail.com>
2022-06-27 20:29:28 -0300

    Rotation detection

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cmath import tan
 import numpy as np
 import cv2 as cv
 import math
@@ -26,27 +27,37 @@ while cap.isOpened():
             cv.polylines(frame, item_corners, True, (0, 0, 255), 4, cv.LINE_AA)
             #print(item_corners, item_ids)
             top_left = item_corners[0][0]
+            top_right = item_corners[0][1]
             bottom_right = item_corners[0][2]
+            bottom_left = item_corners[0][3]
             #print("=========")
             #print("Top Left -", top_left)
             #print("Top Right -", item_corners[0][1])
             #print("Bottom Right -", bottom_right)
             #print("Bottom Left -", item_corners[0][3])
             coordinates_x = [top_left[0], bottom_right[0]]
-            coordinates_x.sort()
+            coordinates_x_sorted = coordinates_x
+            coordinates_x_sorted.sort()
             #print("Coordinates X -", coordinates_x)
             coordinates_y = [top_left[1], bottom_right[1]]
-            coordinates_y.sort()
+            coordinates_y_sorted = coordinates_y
+            coordinates_y_sorted.sort()
             #print("Coordinates Y -", coordinates_y)
-            x_delta = coordinates_x[1] - coordinates_x[0]
-            y_delta = coordinates_y[1] - coordinates_y[0]
+            x_delta = coordinates_x_sorted[1] - coordinates_x_sorted[0]
+            y_delta = coordinates_y_sorted[1] - coordinates_y_sorted[0]
             #square_area = x_length * y_length
             diagonal_length = x_delta * math.sqrt(2)
             #print("Diagonal -", diagonal_length)
 
+            tangent = - (bottom_right[1] - bottom_left[1]) / (bottom_right[0] - bottom_left[0])
+
+            rotation = math.degrees(math.atan(tangent))
+
             if (diagonal_length <= diagonal_threshold) and not stamped:
-                stamp_location_coordinates = [coordinates_x[0] + (x_delta / 2), coordinates_y[0] + (y_delta / 2)]
-                print("Stamp coordinates -", stamp_location_coordinates)
+                stamp_location_coordinates = [coordinates_x_sorted[0] + (x_delta / 2), coordinates_y_sorted[0] + (y_delta / 2)]
+                print("=========")
+                print("Stamp coordinates:", stamp_location_coordinates)
+                print("Rotation:", rotation)
                 stamped = True
             elif (diagonal_length > diagonal_threshold) and stamped:
                 stamped = False
